55bceade0c7e1651ac398480ac2f74614aebee31
Julie Cao <juliehongcao@gmail.com>
2023-06-20 20:57:28 -0700

    [06/20/23] leetcode: summary ranges (fixed order)

--- 228.py
+++ 228.py
@@ -1,12 +1,12 @@
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
         range_lst = []
-        if not nums:
-            return range_lst
-
         start = nums[0]
         end = nums[0]
         length = len(nums)
+        
+        if not nums:
+            return range_lst
 
         for i in range(1, length):
             if nums[i] == end + 1:
